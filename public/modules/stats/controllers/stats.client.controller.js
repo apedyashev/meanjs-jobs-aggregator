78e274737c6fef1b21f2ab247f5da359db6ef3de
apedyashev <alexey.pedyashev@gmail.com>
2015-06-20 11:18:41 +0200

    Stats displaying

--- public/modules/stats/controllers/stats.client.controller.js
+++ public/modules/stats/controllers/stats.client.controller.js
@@ -8,6 +8,15 @@ angular.module('stats').controller('StatsController', ['$scope', 'Job',
 			availabilities: true
 		}).$promise.then(function(stats) {
 			$scope.stats = stats;
+			$scope.maxCityCount = 0;
+			angular.forEach(stats.cities, function(city) {
+				$scope.maxCityCount = (city.count > $scope.maxCityCount) ? city.count : $scope.maxCityCount;
+			});
+
+			$scope.maxAvailCount = 0;
+			angular.forEach(stats.availabilities, function(avail) {
+				$scope.maxAvailCount = (avail.count > $scope.maxAvailCount) ? avail.count : $scope.maxAvailCount;
+			});
 			$scope.loadInProgress = false;
 		}, function() {
 			$scope.loadInProgress = false;
