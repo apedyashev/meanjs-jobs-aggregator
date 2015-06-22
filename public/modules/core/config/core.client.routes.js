eab2c920d573e4fd1c3fafaefc18309a5bb4e0a6
apedyashev <alexey.pedyashev@gmail.com>
2015-06-22 12:40:09 +0200

    Styling of homepage

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -11,6 +11,12 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+			//views: {
+			//	// syntax: <view-name@state-name>. In this case view name is looking in the application shell
+			//	'banner@': {
+			//		templateUrl: 'modules/core/views/home.client.view.html'
+			//	}
+			//}
 		});
 	}
 ]);
