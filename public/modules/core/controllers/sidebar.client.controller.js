df4168f93f151fdb511d53c2700a74802beceef6
apedyashev <alexey.pedyashev@gmail.com>
2015-06-22 18:36:45 +0200

    Responsivness fixes

--- public/modules/core/controllers/sidebar.client.controller.js
+++ public/modules/core/controllers/sidebar.client.controller.js
@@ -4,5 +4,14 @@ angular.module('core').controller('SidebarController', ['$scope',
 	function($scope) {
 		// Sidebar controller logic
 		// ...
+
+		$scope.toggleMenu = function() {
+			$scope.isMenuVisible = !$scope.isMenuVisible;
+		};
+
+		// Collapsing the menu after navigation
+		$scope.$on('$stateChangeSuccess', function() {
+			$scope.isMenuVisible = false;
+		});
 	}
 ]);
