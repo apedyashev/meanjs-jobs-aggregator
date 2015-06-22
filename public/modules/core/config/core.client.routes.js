f334db035b4f95f6c488af07a15ea5f64ee3351b
apedyashev <alexey.pedyashev@gmail.com>
2015-06-22 12:56:22 +0200

    Styling of success- buttons and labels

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -11,12 +11,6 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
-			//views: {
-			//	// syntax: <view-name@state-name>. In this case view name is looking in the application shell
-			//	'banner@': {
-			//		templateUrl: 'modules/core/views/home.client.view.html'
-			//	}
-			//}
 		});
 	}
 ]);
