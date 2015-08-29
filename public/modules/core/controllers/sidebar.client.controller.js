69779e927d46ac1056f0c06281720d78cc09f8bf
apedyashev <alexey.pedyashev@gmail.com>
2015-08-29 15:42:40 +0200

    Fixed issue with adding the active state for menu and sidebar items

--- public/modules/core/controllers/sidebar.client.controller.js
+++ public/modules/core/controllers/sidebar.client.controller.js
@@ -1,17 +1,24 @@
 'use strict';
 
-angular.module('core').controller('SidebarController', ['$scope',
-	function($scope) {
+angular.module('core').controller('SidebarController', ['$scope', '$location',
+	function($scope, $location) {
 		// Sidebar controller logic
 		// ...
 
+		// (For mobile devices ONLY)
 		$scope.toggleMenu = function() {
 			$scope.isMenuVisible = !$scope.isMenuVisible;
 		};
 
-		// Collapsing the menu after navigation
+		// Collapsing the menu after navigation (For mobile devices ONLY)
 		$scope.$on('$stateChangeSuccess', function() {
 			$scope.isMenuVisible = false;
 		});
+
+		// TODO: temporary solution. Must be isItemActive
+		// see http://stackoverflow.com/questions/32286671/why-directives-method-is-available-from-parent-scope
+		$scope.isActive = function (section) {
+			return ($location.path() === section);
+		};
 	}
 ]);
