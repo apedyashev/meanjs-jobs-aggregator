268bbb32662f7f601ac52ec54ef7218d68fa935e
apedyashev <alexey.pedyashev@gmail.com>
2015-10-18 17:04:07 +0200

    Added delete method to allowed methods

--- app/middleware/api.js
+++ app/middleware/api.js
@@ -2,8 +2,8 @@ var cors = require('cors');
 
 module.exports = function apiHeaders(req, res, next) {
     var corsOptions = {
-        methods: ['GET', 'PUT', 'POST'],
-        allowedHeaders: ['Content-Type', '*'],
+        methods: ['GET', 'PUT', 'POST', 'DELETE'],
+        allowedHeaders: 'Content-Type, *',
         credentials: true
     };
     cors(corsOptions)(req, res, next);
