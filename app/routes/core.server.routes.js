f8fb5459f3aff9a95563e04f0f0004b988fc6f28
apedyashev <alexey.pedyashev@gmail.com>
2015-10-19 15:47:00 +0200

    Added the OPTIONS verb support

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -3,8 +3,14 @@
 module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core.server.controller'),
-		indexRoutes = ['/', '/signup', '/signin', '/dashboard*', '/settings*', '/statistics'];
+		indexRoutes = ['/', '/signup', '/signin', '/dashboard*', '/settings*', '/statistics'],
+		apiMiddleware = require('../middleware/api');
+
 	indexRoutes.forEach(function(route) {
 		app.route(route).get(core.index);
+	});
+
+	app.route('*').options(apiMiddleware, function(req, res) {
+		res.json();
 	});
 };
