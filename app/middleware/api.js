ba7d580ab34af434bfdf233c7765f6183c8b500f
apedyashev <alexey.pedyashev@gmail.com>
2015-10-19 14:21:37 +0200

    Allowing of OPTIONS for CORS

--- app/middleware/api.js
+++ app/middleware/api.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function apiHeaders(req, res, next) {
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type,*');
