b37b3ad50e0dc933ddb2af6597ee1ff373fbd9e7
apedyashev <alexey.pedyashev@gmail.com>
2015-12-13 20:15:35 +0100

    Fixed error caused API test fail

--- app/middleware/api.js
+++ app/middleware/api.js
@@ -3,7 +3,7 @@
 module.exports = function apiHeaders(req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type,*');
 
     next();
